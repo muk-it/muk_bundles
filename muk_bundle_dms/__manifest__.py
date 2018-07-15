8d85040a7227e347a80be528e2cb1755552fbacb
Mathias Markl <mathias.markl@mukit.at>
2018-07-16 00:30:03 +0200

    dms_bundle

---
+++ muk_bundle_dms/__manifest__.py
@@ -0,0 +1,68 @@
+###################################################################################
+# 
+#    Copyright (C) 2017 MuK IT GmbH
+#
+#    Odoo Proprietary License v1.0
+#    
+#    This software and associated files (the "Software") may only be used 
+#    (executed, modified, executed after modifications) if you have
+#    purchased a valid license from the authors, typically via Odoo Apps,
+#    or if you have received a written agreement from the authors of the
+#    Software (see the COPYRIGHT file).
+#    
+#    You may develop Odoo modules that use the Software as a library 
+#    (typically by depending on it, importing it and using its resources),
+#    but without copying any source code or material from the Software.
+#    You may distribute those modules under the license of your choice,
+#    provided that this license is compatible with the terms of the Odoo
+#    Proprietary License (For example: LGPL, MIT, or proprietary licenses
+#    similar to this one).
+#    
+#    It is forbidden to publish, distribute, sublicense, or sell copies of
+#    the Software or modified copies of the Software.
+#    
+#    The above copyright notice and this permission notice must be included
+#    in all copies or substantial portions of the Software.
+#    
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#    DEALINGS IN THE SOFTWARE.
+#
+###################################################################################
+
+{
+    "name": "MuK Documents Bundle",
+    "summary": """Document Management System""",
+    "version": '11.0.1.0.0',   
+    "category": 'Document Management',   
+    "license": "OPL-1",
+    "price": 5.00,
+    "currency": 'EUR',
+    "website": "https://www.mukit.at",
+    "live_test_url": "https://demo.mukit.at/web/login",
+    "author": "MuK IT",
+    "contributors": [
+        "Mathias Markl <mathias.markl@mukit.at>",
+    ],
+    "depends": [
+        
+    ],
+    "data": [
+    ],
+    "qweb": [
+        "static/src/xml/*.xml",
+    ],
+    "images": [
+        'static/description/banner.png'
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "application": True,
+    "installable": True,
+}
---
+++ muk_bundle_dms/doc/index.rst
@@ -0,0 +1,55 @@
+================
+Documents Bundle
+================
+
+MuK Documents is a module to create, manage and view files directly within Odoo.
+This bundle includes extensions to release the full potential and fully integrate
+the Documents app into Odoo.
+
+Installation
+============
+
+To install this module, you need to:
+
+Download the module and add it to your Odoo addons folder. Afterward, log on to
+your Odoo server and go to the Apps menu. Trigger the debug modus and update the
+list by clicking on the "Update Apps List" link. Now install the module by
+clicking on the install button.
+
+Configuration
+=============
+
+No additional configuration is needed to use this module.
+
+Usage
+=============
+
+After installation a new app called Documents will appear in Odoo.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Mathias Markl <mathias.markl@mukit.at>
+
+Images
+------------
+
+Some pictures are based on or inspired by:
+
+* `Roundicons <https://www.flaticon.com/authors/roundicons>`_
+* `Smashicons <https://www.flaticon.com/authors/smashicons>`_
+
+Author & Maintainer
+-------------------
+
+This module is maintained by the `MuK IT GmbH <https://www.mukit.at/>`_.
+
+MuK IT is an Austrian company specialized in customizing and extending Odoo.
+We develop custom solutions for your individual needs to help you focus on
+your strength and expertise to grow your business.
+
+If you want to get in touch please contact us via mail
+(sale@mukit.at) or visit our website (https://mukit.at).
