ff837eb3e4adadbfb141ca7d0953d6bc352ab049
MuK IT GmbH <mathias.markl@mukit.at>
2019-01-19 23:40:07 +0000

    publish muk_bundle_theme - 12.0

--- muk_bundle_theme/__manifest__.py
+++ muk_bundle_theme/__manifest__.py
@@ -37,7 +37,7 @@
 {
     "name": "MuK Theme Bundle",
     "summary": """Odoo Community Backend Theme""",
-    "version": '11.0.1.0.0',   
+    "version": '12.0.1.0.0',   
     'category': 'Extra Tools',
     "price": 5.00,
     'currency': 'EUR',
